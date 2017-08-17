2f678cb8abdac6df4bb73a1614a7dfbe8f8fd670
AshwiniKV <a.venkatasubramaniam.1@research.gla.ac.uk>
2017-08-17 23:18:33 +0100

    website

--- build_site.R
+++ build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("/Users/Nick/personal_site")
+setwd("C:\\Users\\user\\Documents\\GitHub\\AshwiniKV.github.io")
 
 #render your sweet site. 
 rmarkdown::render_site()
