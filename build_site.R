0b77c14900f337ead4ad0ce815feeab0def8b720
Ashwini <a.venkatasubramaniam.1@research.gla.ac.uk>
2017-10-31 11:21:44 +0000

    Added research

--- build_site.R
+++ build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("C:\\Users\\user\\Documents\\GitHub\\AshwiniKV.github.io")
+setwd("GitHub\\AshwiniKV.github.io")
 
 #render your sweet site. 
 rmarkdown::render_site()
